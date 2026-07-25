ded5bc8507858bbc4e593b8a4717f1faaeb87640
Llamo <abissfreud@gmail.com>
2026-07-25 23:26:35 +0300

    Add more signer tests

--- internal/signer/signer.go
+++ internal/signer/signer.go
@@ -1,3 +1,5 @@
+// Package signer implements Signer objects which implements methods for
+// loading and generating private keys, and root and leaf certificates
 package signer
 
 import (
@@ -7,38 +9,168 @@ import (
 	"crypto/x509"
 	"crypto/x509/pkix"
 	"encoding/pem"
+	"errors"
 	"fmt"
-	"io"
 	"log/slog"
 	"math/big"
 	u "net/url"
 	"os"
+	"sync"
 	"time"
 
 	lru "github.com/hashicorp/golang-lru/v2"
 )
 
-const KeySize int = 2048
-const MaxCacheSize int = 128
+/* Errors */
 
+var (
+	ErrPKIsNotRSA                        = errors.New("loaded PK is not an RSA PK")
+	ErrPKFileReadFailed                  = errors.New("failed to read bytes from the private key file")
+	ErrCAFileReadFailed                  = errors.New("failed to read bytes from the certificate file")
+	ErrInvalidPKPEMBlock                 = errors.New("loaded private key is invalid")
+	ErrInvalidCertificatePEMBlock        = errors.New("loaded certificate is invalid")
+	ErrCAParsingFailed                   = errors.New("failed to parse root certificate")
+	ErrPKParsingFailed                   = errors.New("failed to parse private key from PKCS#8")
+	ErrSerialNumberGenerationFailed      = errors.New("failed to generate a serial number for a root certificate")
+	ErrRootCertificateCreationFailed     = errors.New("failed to create root certificate")
+	ErrFailedPKMarshal                   = errors.New("failed to marshal PKCS#8 private key")
+	ErrRootCertificateFileCreationFailed = errors.New("failed to create root certificate file")
+	ErrCertificatePEMEncodingFailed      = errors.New("failed to write root certificate using PEM encoding to a file")
+	ErrPKFileCreationFailed              = errors.New("failed to open/create private key file")
+	ErrPKPEMEncodingFailed               = errors.New("failed to write private key using PEM encoding to a file")
+	ErrPKGenerationFailed                = errors.New("failed to generate RSA private key")
+	ErrCacheInitFailed                   = errors.New("failed to initialize LRU cache")
+	ErrLeafCertificateGenerationFailed   = errors.New("failed to create leaf certificate")
+)
+
+/* Signer Type */
+
+// Signer generates certificates and primary keys
 type Signer struct {
-	Cert  *x509.Certificate
-	Pk    *rsa.PrivateKey
-	cache *lru.Cache[string, *tls.Certificate]
+	Cert   *x509.Certificate
+	Pk     *rsa.PrivateKey
+	logger *slog.Logger
+	cache  *lru.Cache[string, *tls.Certificate]
+	mu     sync.RWMutex
 }
 
-func (signer *Signer) String() string {
-	return fmt.Sprintf("{%v,\t%v}", signer.Cert, signer.Pk)
+// Option represents functions that are used for initial configuration of a Signer object
+type Option func(*Signer)
+
+/* Signer Options */
+
+// WithLogger Option that is used to set a signer.logger to a Signer
+// if passed signer.logger pointer is nil, then logging messages are discarded
+func WithLogger(logger *slog.Logger) Option {
+	return func(signer *Signer) {
+		if logger != nil {
+			signer.logger = logger
+		}
+	}
 }
 
-func New(certPath, keyPath string) (signer *Signer, err error) {
-	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
-	if err != nil {
-		slog.Error("Couldn't generate a serial number for a root certificate")
+// WithCache Option that is used to set a LRU cache size to a Signer
+// if given size argument is zero, then no cache is used
+func WithCache(size int) Option {
+	return func(signer *Signer) {
+		if size <= 0 {
+			signer.cache = nil
+			return
+		}
+		cache, err := lru.New[string, *tls.Certificate](size)
+		if err == nil {
+			signer.cache = cache
+		} else {
+			signer.cache = nil
+		}
+	}
+}
+
+/* Signer Methods */
+
+// New creates a new Signer object and returns pointer to it
+// accepts pathes to certificate and key files, PK size in bits, and Option functions for configuration
+func New(certPath, keyPath string, keySize int, opts ...Option) (signer *Signer, err error) {
+	signer = &Signer{
+		logger: slog.New(slog.DiscardHandler),
+	}
+
+	for _, opt := range opts {
+		opt(signer)
+	}
+
+	// Try to load both root certificate and private key
+	if signer.LoadCA(certPath) == nil &&
+		signer.LoadPK(keyPath) == nil &&
+		signer.Cert != nil && signer.Pk != nil &&
+		signer.Cert.IsCA && signer.Cert.BasicConstraintsValid {
+
+		pub, ok := signer.Cert.PublicKey.(*rsa.PublicKey)
+		if ok && pub.Equal(&signer.Pk.PublicKey) {
+			signer.logger.Debug("Loaded existing CA and private key")
+			return
+		}
+	}
+
+	// Generate a new root certificate
+	if err = signer.GenerateCA(); err != nil {
 		return
 	}
 
-	signer = &Signer{}
+	// Generate a new private key
+	if err = signer.GeneratePK(keySize); err != nil {
+		return
+	}
+
+	// Save both certificate and PK at given pathes
+	if err = signer.Save(certPath, keyPath); err != nil {
+		return
+	}
+
+	signer.logger.Debug("Successfully generated and saved signer")
+	return
+}
+
+// LoadCA loads a root certificate from the given path
+func (signer *Signer) LoadCA(certPath string) (err error) {
+	// Read the certificate file bytes
+	certPEMBytes, err := os.ReadFile(certPath)
+	if err != nil {
+		err = fmt.Errorf("%w: %s", ErrCAFileReadFailed, err)
+		signer.logger.Debug("", "error", err)
+		return
+	}
+
+	// Parse certificate PEM bytes into certificate object
+	certBlock, _ := pem.Decode(certPEMBytes)
+	if certBlock == nil {
+		err = ErrInvalidCertificatePEMBlock
+		signer.logger.Error("", "error", err)
+		return
+	}
+
+	cert, err := x509.ParseCertificate(certBlock.Bytes)
+	if err != nil {
+		err = fmt.Errorf("%w: %s", ErrCAParsingFailed, err)
+		signer.logger.Error("", "error", err)
+		return
+	}
+
+	signer.Cert = cert
+	return
+}
+
+// GenerateCA generates and assigns a new root certificate
+func (signer *Signer) GenerateCA() (err error) {
+	// Generate random serial number for the root certificate
+	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
+	if err != nil {
+		err = fmt.Errorf("%w: %s", ErrSerialNumberGenerationFailed, err)
+		signer.logger.Error("", "error", err)
+		return
+	}
+
+	// Create the root certificate object
 	signer.Cert = &x509.Certificate{
 		Version:      3,
 		SerialNumber: serialNumber,
@@ -53,54 +185,57 @@ func New(certPath, keyPath string) (signer *Signer, err error) {
 		MaxPathLen:            1,
 	}
 
-	if err = signer.LoadPK(keyPath); err == nil {
-		slog.Debug("Successfully loaded signer")
-		return
-	} else {
-		slog.Debug("Couldn't load private key for the signer")
-	}
-
-	if err = signer.GeneratePK(); err != nil {
-		slog.Error("Couldn't generate a private key for the signer")
-		return
-	}
-
-	if err = signer.Save(certPath, keyPath); err != nil {
-		slog.Error("Couldn't save the private key and certificate of the signer")
-		return
-	}
-
-	slog.Debug("Successfully generated and saved signer")
 	return
 }
 
+// LoadPK loads a private key from the given file path
 func (signer *Signer) LoadPK(keyPath string) (err error) {
-	pkFile, err := os.Open(keyPath)
+	// Reading bytes from the private key file
+	pkPEMBytes, err := os.ReadFile(keyPath)
 	if err != nil {
-		slog.Debug("Couldn't open private key file")
+		err = fmt.Errorf("%w: %s", ErrPKFileReadFailed, err)
+		signer.logger.Debug("", "error", err)
 		return
 	}
 
-	pkPemBytes, err := io.ReadAll(pkFile)
-	if err != nil {
-		slog.Error("Couldn't read data from private key file")
+	// Decode private key from PEM bytes to RSA PK
+	pkBlock, _ := pem.Decode(pkPEMBytes)
+	if pkBlock == nil {
+		err = ErrInvalidPKPEMBlock
+		signer.logger.Error("", "error", err)
 		return
 	}
 
-	pkBlock, _ := pem.Decode(pkPemBytes)
-	pkBytes := pkBlock.Bytes
-
-	pk, err := x509.ParsePKCS8PrivateKey(pkBytes)
+	pk, err := x509.ParsePKCS8PrivateKey(pkBlock.Bytes)
 	if err != nil {
-		slog.Error("Couldn't parse private key from PKCS#8")
+		err = fmt.Errorf("%w: %s", ErrPKParsingFailed, err)
+		signer.logger.Error("", "error", err)
 		return
 	}
-	signer.Pk = pk.(*rsa.PrivateKey)
+	var ok bool
+	signer.Pk, ok = pk.(*rsa.PrivateKey)
+	if !ok {
+		err = ErrPKIsNotRSA
+		signer.logger.Error("", "error", err)
+		return
+	}
 
-	slog.Debug("Successfully loaded private key")
+	signer.logger.Debug("Successfully loaded private key")
 	return
 }
 
+// GeneratePK generates and assigns a new random RSA private key of the given size
+func (signer *Signer) GeneratePK(keySize int) error {
+	// Generate random private key
+	pk, err := rsa.GenerateKey(rand.Reader, keySize)
+	if err != nil {
+		return fmt.Errorf("%w: %s", ErrPKGenerationFailed, err)
+	}
+	signer.Pk = pk
+	return nil
+}
+
+// Save writes certificate and private key to the given pathes
 func (signer *Signer) Save(certPath, keyPath string) error {
 	// Certificate bytes
 	certBytes, err := x509.CreateCertificate(
@@ -111,102 +246,98 @@ func (signer *Signer) Save(certPath, keyPath string) error {
 		signer.Pk,
 	)
 	if err != nil {
-		return fmt.Errorf("couldn't create root certificate: %s", err)
+		return fmt.Errorf("%w: %s", ErrRootCertificateCreationFailed, err)
 	}
 
 	// Private key bytes
 	pkBytes, err := x509.MarshalPKCS8PrivateKey(signer.Pk)
 	if err != nil {
-		return fmt.Errorf("couldn't marshal PKCS#8 private key: %s", err)
+		return fmt.Errorf("%w: %s", ErrFailedPKMarshal, err)
 	}
 
-	// Save certificate
+	// Creating or truncating the certificate file
 	certFile, err := os.Create(certPath)
 	if err != nil {
-		return fmt.Errorf("couldn't open/create root certificate file: %s", err)
+		return fmt.Errorf("%w: %s", ErrRootCertificateFileCreationFailed, err)
 	}
 	defer certFile.Close()
 
-	block := pem.Block{
+	// Creating PEM block object for certificate
+	certBlock := pem.Block{
 		Type:  "CERTIFICATE",
 		Bytes: certBytes,
 	}
 
-	err = pem.Encode(certFile, &block)
+	// Writing encoded PEM certificate block to the certificate file
+	err = pem.Encode(certFile, &certBlock)
 	if err != nil {
-		return fmt.Errorf("couldn't write root certificate using PEM encoding to a file: %s", err)
+		return fmt.Errorf("%w: %s", ErrCertificatePEMEncodingFailed, err)
 	}
 
-	// Save private key
+	// Creating or truncating the private key file
 	keyFile, err := os.Create(keyPath)
 	if err != nil {
-		return fmt.Errorf("couldn't open/create private key file: %s", err)
+		return fmt.Errorf("%w: %s", ErrPKFileCreationFailed, err)
 	}
 	defer keyFile.Close()
 
-	block = pem.Block{
-		Type:  "RSA PRIVATE KEY",
+	// Creating PEM block object for private key
+	pkBlock := pem.Block{
+		Type:  "PRIVATE KEY",
 		Bytes: pkBytes,
 	}
 
-	err = pem.Encode(keyFile, &block)
+	// Writing encoded PEM private key block to the PK file
+	err = pem.Encode(keyFile, &pkBlock)
 	if err != nil {
-		return fmt.Errorf("couldn't write private key using PEM encoding to a file: %s", err)
+		return fmt.Errorf("%w: %s", ErrPKPEMEncodingFailed, err)
 	}
 
 	return nil
 }
 
-func (signer *Signer) GeneratePK() error {
-	pk, err := rsa.GenerateKey(rand.Reader, KeySize)
-	if err != nil {
-		return fmt.Errorf("couldn't generate RSA private key: %s", err)
-	}
-	signer.Pk = pk
-	return nil
-}
-
-func (signer *Signer) GenerateCertificate(url u.URL) (*tls.Certificate, error) {
+// GenerateLeafCertificate generates a new leaf certificate for the given URL of the given size
+func (signer *Signer) GenerateLeafCertificate(url u.URL, keySize int) (*tls.Certificate, error) {
 	hostname := url.Hostname()
 
-	if signer.cache == nil {
-		cache, err := lru.New[string, *tls.Certificate](MaxCacheSize)
-		if err != nil {
-			return nil, fmt.Errorf("couldn't initialize LRU cache: %s", err)
-		}
-		signer.cache = cache
-	}
-
-	if signer.cache.Contains(hostname) {
+	// Check if certificate is in cache and if it exists
+	if signer.cache != nil {
+		signer.mu.RLock()
 		cert, ok := signer.cache.Get(hostname)
+		signer.mu.RUnlock()
 		if ok {
-			slog.Debug(fmt.Sprintf("Leaf certificate cache hit for %s", hostname))
+			signer.logger.Debug(fmt.Sprintf("Leaf certificate cache hit for %s", hostname))
 			return cert, nil
 		}
 	}
 
-	pk, err := rsa.GenerateKey(rand.Reader, KeySize)
+	// Generate a new random private key
+	pk, err := rsa.GenerateKey(rand.Reader, keySize)
 	if err != nil {
-		return nil, fmt.Errorf("couldn't generate private key of a leaf certificate: %s", err)
+		return nil, fmt.Errorf("%w: %s", ErrPKGenerationFailed, err)
 	}
 
+	// Generate a random serial number for a new leaf certificate
 	serialNumber, err := rand.Int(rand.Reader, new(big.Int).Lsh(big.NewInt(1), 128))
 	if err != nil {
-		return nil, fmt.Errorf("couldn't generate serial number of a leaf certificate: %s", err)
+		return nil, fmt.Errorf("%w: %s", ErrSerialNumberGenerationFailed, err)
 	}
+
+	// Generate a leaf certificate
 	cert := x509.Certificate{
 		Version:      3,
 		SerialNumber: serialNumber,
 		Subject: pkix.Name{
-			CommonName: url.Hostname(),
+			CommonName: hostname,
 		},
 		NotBefore:   time.Now().Add(-time.Hour),
 		NotAfter:    time.Now().AddDate(1, 0, 0),
 		KeyUsage:    x509.KeyUsageDigitalSignature,
 		ExtKeyUsage: []x509.ExtKeyUsage{x509.ExtKeyUsageServerAuth},
-		DNSNames:    []string{url.Hostname()},
+		DNSNames:    []string{hostname},
 	}
 
+	// Sign the leaf certificate and get its bytes
 	certBytes, err := x509.CreateCertificate(
 		rand.Reader,
 		&cert,
@@ -214,17 +345,22 @@ func (signer *Signer) GenerateCertificate(url u.URL) (*tls.Certificate, error) {
 		&pk.PublicKey,
 		signer.Pk,
 	)
-
 	if err != nil {
-		return nil, fmt.Errorf("couldn't create leaf certificate: %s", err)
+		return nil, fmt.Errorf("%w: %s", ErrLeafCertificateGenerationFailed, err)
 	}
 
+	// Create certificate object for TLS connection
 	tlsCert := tls.Certificate{
-		Certificate: [][]byte{certBytes},
+		Certificate: [][]byte{certBytes, signer.Cert.Raw},
 		PrivateKey:  pk,
 	}
 
-	signer.cache.Add(hostname, &tlsCert)
+	// Add TLS certificate to cache if it exists
+	if signer.cache != nil {
+		signer.mu.Lock()
+		signer.cache.Add(hostname, &tlsCert)
+		signer.mu.Unlock()
+	}
 
 	return &tlsCert, nil
 }
--- internal/signer/signer_test.go
+++ internal/signer/signer_test.go
@@ -1,68 +1,259 @@
 package signer
 
 import (
+	"bytes"
+	"errors"
+	"log/slog"
 	"os"
+	"path/filepath"
+	"strings"
 	"testing"
+
+	u "net/url"
 )
 
-func getSignerFilePathes(t *testing.T) (certPath, keyPath string) {
-	t.Helper()
-
-	//
-	certFile, err := os.CreateTemp("", "test-certificate-*.cert")
-	if err != nil {
-		t.Fatalf("Couldn't create a temporary certificate file")
-	}
-	certPath = certFile.Name()
-	certFile.Close()
-	os.Remove(certPath)
-
-	//
-	keyFile, err := os.CreateTemp("", "test-keylist-*.txt")
-	if err != nil {
-		t.Fatalf("Couldn't create a temporary key file")
-	}
-	keyPath = keyFile.Name()
-	keyFile.Close()
-	os.Remove(keyPath)
-	return
-}
+// Test Config
+const testPKSize = 2048
 
 // Create Signer
 func TestCreateSigner(t *testing.T) {
-	certPath, keyPath := getSignerFilePathes(t)
-	_, err := New(certPath, keyPath)
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	_, err := New(certPath, keyPath, testPKSize)
 	if err != nil {
 		t.Fatalf("Failed to create a signer: %s", err)
 	}
-	defer os.Remove(certPath)
-	defer os.Remove(keyPath)
 }
 
-// Load Signer
-func TestLoadSigner(t *testing.T) {
-	certPath, keyPath := getSignerFilePathes(t)
-	signer, err := New(certPath, keyPath)
+// Create a Signer with a logger
+func TestCreateSignerWithLogger(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	var b strings.Builder
+	logger := slog.New(slog.NewTextHandler(&b, nil))
+	_, err := New(certPath, keyPath, testPKSize, WithLogger(logger))
 	if err != nil {
 		t.Fatalf("Failed to create a signer: %s", err)
 	}
-	defer os.Remove(certPath)
-	defer os.Remove(keyPath)
 
-	// Saving signer
-	err = signer.Save(certPath, keyPath)
+	if !strings.Contains(b.String(), "Successfully generated and saved signer") {
+		t.Fatalf("Success string isn't written to the logger")
+	}
+}
+
+// Create a Signer with a cache
+func TestCreateSignerWithCache(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize, WithCache(8))
 	if err != nil {
-		t.Fatalf("Failed to save a signer: %s", err)
+		t.Fatalf("Failed to create a signer: %s", err)
 	}
 
-	// Load signer
-	signerLoaded, err := New(certPath, keyPath)
+	if signer.cache == nil {
+		t.Fatalf("Failed to create a LRU cache")
+	}
+}
+
+// Create a Signer with a cache with size 0 or smaller
+func TestCreateSignerWithNoCache(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize, WithCache(-1))
 	if err != nil {
-		t.Fatalf("Failed to load a signer: %s", err)
+		t.Fatalf("Failed to create a signer: %s", err)
 	}
 
-	// Different certificate is fine
-	if !signerLoaded.Pk.Equal(signer.Pk) {
-		t.Fatalf("Saved and loaded signers have different private keys")
+	if signer.cache == nil {
+		t.Fatalf("Failed to create a LRU cache")
+	}
+}
+
+// Load a root certificate
+func TestLoadCertificate(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	cert := *signer.Cert
+	signer.Cert = nil
+	err = signer.LoadCA(certPath)
+	if err != nil {
+		t.Fatalf("Failed to load a root certificate: %s", err)
+	}
+
+	if !cert.Equal(signer.Cert) {
+		t.Fatalf("Loaded certificate isn't equal to the saved root certificate")
+	}
+}
+
+// Load a root certificate with an invalid PEM block
+func TestIvalidPEMLoadCertificate(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	err = os.WriteFile(certPath, []byte("Invalid PEM"), 0644)
+	if err != nil {
+		t.Fatalf("Failed to write to a file: %s", err)
+	}
+
+	err = signer.LoadCA(certPath)
+	if err == nil {
+		t.Fatalf("Didn't return an error on invalid certificate")
+	}
+
+	if !errors.Is(err, ErrInvalidCertificatePEMBlock) {
+		t.Fatalf("Error returned wasn't for invalid PEM block: %s", err)
+	}
+}
+
+// Generate and assign a new root certificate
+func TestGenerateCertificate(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	err = signer.GenerateCA()
+	if err != nil {
+		t.Fatalf("Failed to generate a new root certificate: %s", err)
+	}
+
+	if signer.Cert == nil || !signer.Cert.IsCA {
+		t.Fatalf("Generated root certificate is invalid")
+	}
+}
+
+// Load a private key file
+func TestLoadPK(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	pk := *signer.Pk
+	signer.Pk = nil
+	err = signer.LoadPK(keyPath)
+	if err != nil {
+		t.Fatalf("Failed to load a private key: %s", err)
+	}
+
+	if !pk.Equal(signer.Pk) {
+		t.Fatalf("Loaded private key isn't equal to the saved private key")
+	}
+}
+
+// Load a private key file with an invalid PEM block
+func TestInvalidPEMLoadPK(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	err = os.WriteFile(keyPath, []byte("Invalid PEM"), 0644)
+
+	err = signer.LoadPK(keyPath)
+	if err == nil {
+		t.Fatalf("Didn't return an error on invalid certificate")
+	}
+
+	if !errors.Is(err, ErrInvalidPKPEMBlock) {
+		t.Fatalf("Error returned wasn't for invalid PEM block: %s", err)
+	}
+}
+
+// Generate and assign a new random private key
+func TestGeneratePK(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	err = signer.GeneratePK(testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to generate a new private key: %s", err)
+	}
+
+	if signer.Pk == nil || signer.Pk.Size() != testPKSize {
+		t.Fatalf("Generated private key is invalid")
+	}
+}
+
+// Save a private key and a root certficate of a Signer
+func TestSignerSave(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	signerNew, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+	if !signerNew.Pk.Equal(signer.Pk) || !signerNew.Cert.Equal(signer.Cert) {
+		t.Fatalf("Loaded certificate and private key aren't equal to saved")
+	}
+}
+
+// Generate a leaf certificate
+func TestGenerateLeafCertificate(t *testing.T) {
+	dir := t.TempDir()
+	certPath := filepath.Join(dir, "ca.cert")
+	keyPath := filepath.Join(dir, "ca.key")
+
+	signer, err := New(certPath, keyPath, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to create a signer: %s", err)
+	}
+
+	url, err := u.Parse("https://example.com")
+	if err != nil {
+		t.Fatalf("Failed to parse example URL: %s", err)
+	}
+	leafCert, err := signer.GenerateLeafCertificate(*url, testPKSize)
+	if err != nil {
+		t.Fatalf("Failed to generate a leaf certificate: %s", err)
+	}
+	if !bytes.Equal(leafCert.Certificate[1], signer.Cert.Raw) {
+		t.Fatalf("Generated leaf certificate is invalid")
 	}
 }
